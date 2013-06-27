32007923e24e919e45f3c9d23d8d3c4fb8ebe2b6
Praveen Sridhar <prvn431@gmail.com>
2013-06-27 13:44:22 +0530

    third commit

--- js/install.js
+++ js/install.js
@@ -1,4 +1,7 @@
 // Install app
+var ins = document.querySelector("#install");
+ins.style.display = "none";
+$('#install').closest('.ui-btn').hide();
 if (navigator.mozApps) {
     var checkIfInstalled = navigator.mozApps.getSelf();
     checkIfInstalled.onsuccess = function () {
@@ -17,6 +20,8 @@ if (navigator.mozApps) {
         else {
             var install = document.querySelector("#install"),
                 manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
+	    ins.style.display = "true";
+	    $('#install').closest('.ui-btn').show();
             install.className = "show-install";
             install.onclick = function () {
                 var installApp = navigator.mozApps.install(manifestURL);
